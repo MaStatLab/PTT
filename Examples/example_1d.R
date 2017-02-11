ccdd0936ab46e7714de391ea8fc05a2e30b5b2bb
Li Ma <li.ma@duke.edu>
2017-02-11 15:09:47 -0500

    Added 1D example for optional Polya trees.

--- Examples/example_1d.R
+++ Examples/example_1d.R
@@ -15,13 +15,23 @@ true.den = p1*dunif(x) + p2*dunif(x,0.25,0.5) + p3 *4*dbeta((x-0.25)*4,2,2) + p4
 ans = markov.apt.density(X=obs.mat,max.dim=max.dim,rho0=0.2,rho0.mode=0,n.s=3,tran.mode=1,beta=0,lognu.lb=-1,lognu.ub=4,n.grid=5)
 pred.den = markov.apt.density(X=obs.mat,Xpred=x,max.dim=max.dim,rho0=0.2,rho0.mode=0,n.s=3,tran.mode=1,beta=0,lognu.lb=-1,lognu.ub=4,n.grid=5)$pred
 
-
 xlim = c(0,1)
 ylim = c(0,15)
 plot(x,pred.den,type='l',xlim=xlim,ylim=ylim,xlab="x",ylab="Density")
 par(new=TRUE)
 plot(x,true.den,type='l',lty=2,col='red',xlab="",ylab="",xlim=xlim,ylim=ylim)
 
+
+## Try OPT instead
+ans.opt = markov.opt.density(X=obs.mat,max.dim=max.dim,rho0=0.2,rho0.mode=0)
+pred.den.opt = markov.opt.density(X=obs.mat,Xpred=x,max.dim=max.dim,rho0=0.2,rho0.mode=0)$pred
+
+
+xlim = c(0,1)
+ylim = c(0,15)
+plot(x,pred.den.opt,type='l',xlim=xlim,ylim=ylim,xlab="x",ylab="Density")
+par(new=TRUE)
+plot(x,true.den,type='l',lty=2,col='red',xlab="",ylab="",xlim=xlim,ylim=ylim)
 
 
 
