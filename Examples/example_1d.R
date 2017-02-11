893c51be389285d5af4233092a9cc70f548fea12
Li Ma <li.ma@duke.edu>
2017-02-11 15:53:26 -0500

    Resolve package dependency for Example 2D.

--- Examples/example_1d.R
+++ Examples/example_1d.R
@@ -7,13 +7,13 @@ size.max = nobs
 p1=0.1;p2=0.3;p3=0.4;p4=0.2
 sample.ind = runif(size.max)
 
-obs.mat = as.matrix((sample.ind<=p1)*runif(size.max)+(p1<sample.ind & sample.ind<=p1+p2)*(0.25+rbeta(size.max,1,1)*0.25)+(p1+p2<sample.ind & sample.ind<=p1+p2+p3)*(0.25+rbeta(size.max,2,2)*0.25) +(sample.ind>p1+p2+p3)*rbeta(size.max,5000,2000))
+X = as.matrix((sample.ind<=p1)*runif(size.max)+(p1<sample.ind & sample.ind<=p1+p2)*(0.25+rbeta(size.max,1,1)*0.25)+(p1+p2<sample.ind & sample.ind<=p1+p2+p3)*(0.25+rbeta(size.max,2,2)*0.25) +(sample.ind>p1+p2+p3)*rbeta(size.max,5000,2000))
 
 true.den = p1*dunif(x) + p2*dunif(x,0.25,0.5) + p3 *4*dbeta((x-0.25)*4,2,2) + p4*dbeta(x,5000,2000)
 
 ## Fit a Markov APT
-ans = apt(X=obs.mat,max.dim=max.dim,rho0=0.2,rho0.mode=0,n.s=3,tran.mode=1,beta=0,lognu.lb=-1,lognu.ub=4,n.grid=5)
-pred.den = apt(X=obs.mat,Xpred=x,max.dim=max.dim,rho0=0.2,rho0.mode=0,n.s=3,tran.mode=1,beta=0,lognu.lb=-1,lognu.ub=4,n.grid=5)$pred
+ans = apt(X=X,max.dim=max.dim,rho0=0.2)
+pred.den = apt(X=X,Xpred=x,max.dim=max.dim,rho0=0.2)$pred
 
 xlim = c(0,1)
 ylim = c(0,15)
@@ -23,8 +23,8 @@ plot(x,true.den,type='l',lty=2,col='red',xlab="",ylab="",xlim=xlim,ylim=ylim)
 
 
 ## Fit OPT instead
-ans.opt = opt(X=obs.mat,max.dim=max.dim,rho0=0.2,rho0.mode=0)
-pred.den.opt = opt(X=obs.mat,Xpred=x,max.dim=max.dim,rho0=0.2,rho0.mode=0)$pred
+ans.opt = opt(X=X,max.dim=max.dim,rho0=0.2)
+pred.den.opt = opt(X=X,Xpred=x,max.dim=max.dim,rho0=0.2)$pred
 
 
 xlim = c(0,1)
--- Examples/example_2d.R
+++ Examples/example_2d.R
@@ -1,6 +1,3 @@
-
-library(mvtnorm)
-
 transform=FALSE
 x.grid = seq(0.0000001,0.9999999,by=0.01)
 y.grid = seq(0.0000001,0.9999999,by=0.01)
@@ -25,33 +22,28 @@ obs.mat = (sample.ind < p1) * norm.obs.mat + (sample.ind >= p1 ) * norm.obs.mat.
 true.den = function(xy.grid) { p1*dmvnorm(xy.grid,mean=mean,sigma=sigma) + (1-p1)*dmvnorm(xy.grid,mean=mean.local,sigma=sigma.local) }
 true.den.grid = true.den(xy.grid)
 
-
-
-
 n.post.sample=500
 max.dim = 11
 
-library(grid)
-
-markov.apt.2D.fit = apt(obs.mat,max.dim=max.dim,rho0=0.1,rho0.mode=0,n.s=5,tran.mode=2,beta=0,
-                                       lognu.lb=-1,lognu.ub=4,n.grid=5,x.mat.pred=xy.grid,n.post.samples = n.post.sample)
+markov.apt.2D.fit = apt(X=obs.mat,Xpred=xy.grid,max.dim=max.dim,rho0=0.2,
+                        tran.mode=2,beta=0,n.post.samples = n.post.sample)
 
 for (i in 1:n.post.sample) {
 
   part.post.sample = markov.apt.2D.fit$part_points_post_samples[[i]]
 
-terminal.part = part.post.sample[which(part.post.sample[,"nu"] == Inf),]
-part.to.plot = cbind(terminal.part,den = exp(terminal.part[,"logp"])*2^(terminal.part[,"level"]))
-part.to.plot[,2] = part.to.plot[,2]+1
-part.to.plot[,4] = part.to.plot[,4]+1
-part.to.plot[,1:4] = part.to.plot[,1:4] / 2^max.dim
-colnames(part.to.plot) = c("xmin","xmax","ymin","ymax","level","nu","lopp","den")
+  terminal.part = part.post.sample[which(part.post.sample[,"nu"] == Inf),]
+  part.to.plot = cbind(terminal.part,den = exp(terminal.part[,"logp"])*2^(terminal.part[,"level"]))
+  part.to.plot[,2] = part.to.plot[,2]+1
+  part.to.plot[,4] = part.to.plot[,4]+1
+  part.to.plot[,1:4] = part.to.plot[,1:4] / 2^max.dim
+  colnames(part.to.plot) = c("xmin","xmax","ymin","ymax","level","nu","lopp","den")
 
-xlim=c(0,1)
-ylim=c(0,1)
-zlim=c(0,max(part.to.plot[,"den"]))
-border=FALSE
-plot.part(part.to.plot,xlim=xlim,ylim=ylim,zlim=zlim,border=border,plot.scale=FALSE,color.fun=topo.colors)
+  xlim=c(0,1)
+  ylim=c(0,1)
+  zlim=c(0,max(part.to.plot[,"den"]))
+  border=FALSE
+  plot.part(part.to.plot,xlim=xlim,ylim=ylim,zlim=zlim,border=border,plot.scale=FALSE,color.fun=topo.colors)
 
-Sys.sleep(0.5)
+  Sys.sleep(0.5)
 }
