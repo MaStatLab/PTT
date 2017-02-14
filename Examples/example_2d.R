f3c71ef6863535e649a40e3acae944acd92963c0
Li Ma <li.ma@duke.edu>
2017-02-14 15:14:37 -0500

    Remove tests on finding hmap partitions.

--- Examples/example_2d.R
+++ Examples/example_2d.R
@@ -28,10 +28,6 @@ max.resol = 11
 markov.apt.2D.fit = apt(X=obs.mat,Xpred=xy.grid,max.resol=max.resol,rho0=0.2,
                         tran.mode=2,beta=0,n.post.samples = n.post.sample)
 
-# ans1 = opt(cbind(rbeta(100,20,20),runif(100)))
-# ans1$part_points_hmap
-
-
 
 for (i in 1:n.post.sample) {
 
